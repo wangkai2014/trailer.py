5b8117e0069f8332c7c71e14be6da61b731c54af
initedit-project <initeditinitedit@gmail.com>
2018-08-29 16:19:01 +0530

    vduration empty randrange fixed

--- subclip.py
+++ subclip.py
@@ -19,10 +19,12 @@ class subclip:
             self.vlist2 = self.vlist2+list1+str(vcount)+".mp4 "
             print("vduration = ",self.getfileinfo.vduration)
             if self.getfileinfo.vduration >=4 :
-                randomstart=random.randrange(self.getfileinfo.vduration-4)
+                randomstart=random.randrange(self.getfileinfo.vduration-3)
+                randomlen=(random.choice([2,3,4]))
             else :
                 randomstart=0
-            randomlen=(random.choice([2,3,4]))
+                randomlen=self.getfileinfo.vduration
+
             print("ffmpeg -i \""+i+"\" -ss "+str(randomstart)+" -t "+str(randomlen)+" -async 1 "+str(vcount)+".mp4")
             p = subprocess.Popen("ffmpeg -i \""+i+"\" -ss "+str(randomstart)+" -t "+str(randomlen)+" -async 1 "+str(vcount)+".mp4", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             for line in p.stdout.readlines():
