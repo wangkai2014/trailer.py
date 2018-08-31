a2f9ec358620171501f8d6ccc9f3980779811f5a
initedit-project <initeditinitedit@gmail.com>
2018-08-31 20:15:34 +0530

    .mkv added

--- files.py
+++ files.py
@@ -4,13 +4,15 @@ import subprocess
 class files:
     def getfiles(self):
         vlist=[]
-        for name in glob.glob('*.mp4'):
+        for name in glob.glob('*.MP4'):
 	        vlist.append(name)
         for name in glob.glob('*.AVI'):
 	        vlist.append(name)
+        for name in glob.glob('*.MKV'):
+	        vlist.append(name)
         for name in glob.glob('*.MOV'):
 	        vlist.append(name)
-        for name in glob.glob('*.3gp'):
+        for name in glob.glob('*.3GP'):
 	        vlist.append(name)
         print("Selected Videos :",vlist)
         return vlist
