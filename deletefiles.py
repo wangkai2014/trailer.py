3c3c1d660128309bc89a43542cb6ff988012452e
initedit-project <initeditinitedit@gmail.com>
2018-08-31 20:18:52 +0530

    img2mp4

--- deletefiles.py
+++ deletefiles.py
@@ -2,17 +2,21 @@ import os
 import glob
 class deletefiles:
 
-    def getdeletefiles(self,vlist):
+    def getdeletefiles(self,vlist,imglist):
         # Delete tmp files
         print("Deleting tmp files...")
         vcount=0
         for i in vlist:
             vcount=vcount+1
             os.remove(str(vcount)+".mp4")
+
         os.remove("concatenate.mp4")
         os.remove("audiogain.mp4")
         os.remove("addaudio.mp4")
         os.remove("fade.mp4")
+        for i in imglist:
+            os.remove("out."+i+".mp4")
+        
         os.remove("filter.mp4")
         os.remove("output.png")
         print("Delete all *.mp4 files")
