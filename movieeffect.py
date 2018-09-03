7d385f0c3e507b3ed7a797a75e8435f49cffbcb0
initedit-project <initeditinitedit@gmail.com>
2018-09-03 12:48:24 +0530

    Letter box with drawbox

--- movieeffect.py
+++ movieeffect.py
@@ -25,28 +25,19 @@ class movieeffect:
     
     def blackbar(self):
         # Movie black bar
-        # Download input.png if not exsist
-        my_file = Path("input.png")
-        if my_file.exists():
-            print("input.png found")
-        else:
-            print('downloading input.png')
-            url = 'https://github.com/initedit-project/trailer.py/raw/master/input.png'  
-            urllib.request.urlretrieve(url, 'input.png')
-        
         vfilter = input("Get Movie black bar(y/n) ? : ")
-        self.getfileinfo.getvideoinfo("filter.mp4")
         if vfilter=="y" :
-            print("ffmpeg -i input.png -vf scale="+str(self.getfileinfo.w)+":"+str(self.getfileinfo.h)+" output.png")
-            p = subprocess.Popen("ffmpeg -i input.png -vf scale="+str(self.getfileinfo.w)+":"+str(self.getfileinfo.h)+" output.png", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            for line in p.stdout.readlines():
-                print(line),
-            print("ffmpeg -i filter.mp4 -i output.png -filter_complex \"[0:v][1:v] overlay=0:0:enable='between(t,0,"+str(self.getfileinfo.vduration+4)+")'\" -pix_fmt yuv420p -c:a copy Movietrailer.mp4")
-            p = subprocess.Popen("ffmpeg -i filter.mp4 -i output.png -filter_complex \"[0:v][1:v] overlay=0:0:enable='between(t,0,"+str(self.getfileinfo.vduration+4)+")'\" -pix_fmt yuv420p -c:a copy Movietrailer.mp4", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            print("ffmpeg -i filter.mp4 -vf  drawbox=x=0:y=0:w=0:h=200:color=black:t=fill,drawbox=x=0:y=880:w=0:h=200:color=black:t=fill Movietrailer.mp4")
+            p = subprocess.Popen("ffmpeg -i filter.mp4 -vf  drawbox=x=0:y=0:w=0:h=200:color=black:t=fill,drawbox=x=0:y=880:w=0:h=200:color=black:t=fill Movietrailer.mp4", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             for line in p.stdout.readlines():
                 print(line),
         else:
             os.rename("filter.mp4","Movietrailer.mp4")
 
-
+    def nashville(self):
+        #nashville
+        print("adding nashville")
+        p = subprocess.Popen("ffmpeg -i Movietrailer.mp4 -vf drawbox=x=0:y=0:w=1920:h=1080:color=blue@0.1:t=fill,drawbox=x=0:y=0:w=1920:h=1080:color=red@0.05:t=fill filter.mp4", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        for line in p.stdout.readlines():
+            print(line),
 
