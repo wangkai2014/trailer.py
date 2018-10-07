410d8718814cccef4ac80cd33967fe295a439b7c
initedit-project <initeditinitedit@gmail.com>
2018-10-07 12:24:48 +0530

    dynamic img2mp4

--- img2mp4.py
+++ img2mp4.py
@@ -12,12 +12,13 @@ class img2mp4:
         for imgs in glob.glob('*.JPEG.JPG'):
             self.imglist.append(imgs)
 
-        #convert imgs to 1 sec mp4
+        #convert imgs to random sec mp4
 
         for i in self.imglist:
             print("self.imglist :  ",self.imglist)
             print("Working on : "+i)
-            print("ffmpeg -loop 1 -i "+i+" -c:v libx264 -t 1 -pix_fmt yuv420p -vf scale=1920:1080,setdar=16:9,setsar=1:1 out."+i+".mp4")
-            p = subprocess.Popen("ffmpeg -loop 1 -i "+i+" -c:v libx264 -t 1 -pix_fmt yuv420p -vf scale=1920:1080,setdar=16:9,setsar=1:1 out."+i+".mp4", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            rtime=str(random.uniform(0.8, 1))
+            print("ffmpeg -loop 1 -i "+i+" -c:v libx264 -t "+rtime+" -pix_fmt yuv420p -vf scale=1920:1080,setdar=16:9,setsar=1:1 out."+i+".mp4")
+            p = subprocess.Popen("ffmpeg -loop 1 -i "+i+" -c:v libx264 -t "+rtime+" -pix_fmt yuv420p -vf scale=1920:1080,setdar=16:9,setsar=1:1 out."+i+".mp4", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             for line in p.stdout.readlines():
                 print(line),
